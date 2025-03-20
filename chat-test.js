9dd9e5164631ebc2fac2ba141e85d85168a34396
Alvin <alvin12394@gmail.com>
2025-03-21 06:29:57 +0700

    fix wide emote

--- chat-test.js
+++ chat-test.js
@@ -393,7 +393,7 @@ window.addEventListener('onEventReceived', function (obj) {
                   urls: {
                     '1': 'https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp',
                     '2': 'https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp',
-                    '4': 'https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp',
+                    '4': 'https://cdn.7tv.app/emote/01FCX95ZG80009QXBMY7YYTVBJ/1x.webp',
                   },
                   positions: [[0, 8], [10, 18], [20, 28]]
                 },
@@ -403,7 +403,7 @@ window.addEventListener('onEventReceived', function (obj) {
             renderedText:
               '<img src="https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp" srcset="https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp 1x, https://cdn.frankerfacez.com/emoticon/407882/2 2x, https://cdn.frankerfacez.com/emoticon/407882/4 4x" title="CupBunny" class="emote"> ' +
               '<img src="https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp" srcset="https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp 1x, https://cdn.frankerfacez.com/emoticon/407882/2 2x, https://cdn.frankerfacez.com/emoticon/407882/4 4x" title="CupBunny" class="emote"> ' +
-              '<img src="https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp" srcset="https://cdn.7tv.app/emote/01GWDGXVG0000FVNNHM4H1HZRF/1x.webp 1x, https://cdn.frankerfacez.com/emoticon/407882/2 2x, https://cdn.frankerfacez.com/emoticon/407882/4 4x" title="CupBunny" class="emote">',
+              '<img src="https://cdn.7tv.app/emote/01FCX95ZG80009QXBMY7YYTVBJ/1x.webp" srcset="https://cdn.7tv.app/emote/01FCX95ZG80009QXBMY7YYTVBJ/1x.webp 1x, https://cdn.7tv.app/emote/01FCX95ZG80009QXBMY7YYTVBJ/1x.webp 2x, https://cdn.7tv.app/emote/01FCX95ZG80009QXBMY7YYTVBJ/1x.webp 4x" title="CupBunny" class="emote">',
           },
         };
         break;
